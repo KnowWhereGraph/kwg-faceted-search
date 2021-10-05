d8a496f670ea2fba190a1184165fc9283defd2bb
zilongliu <jamesliu820@163.com>
2021-10-04 20:36:28 -0700

    Update index.js

--- dr-faceted-search-app/routes/index.js
+++ dr-faceted-search-app/routes/index.js
@@ -1,5 +1,15 @@
 var express = require('express');
+const path = require('path');
 var router = express.Router();
+
+const P_ENDPOINT = "http://stko-roy.geog.ucsb.edu:7202/sparql";
+
+const P_DIST = path.join(__dirname, '../../public/');
+
+router.use('/libraries', express.static(P_DIST+'/libraries'));
+router.use('/stylesheets', express.static(P_DIST+'/stylesheets'));
+router.use('/javascripts', express.static(P_DIST+'/javascripts'));
+router.use('/images', express.static(P_DIST+'/images'));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
