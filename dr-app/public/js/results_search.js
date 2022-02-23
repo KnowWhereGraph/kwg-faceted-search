7e22df6669a8411ab0dd715f6af40c6586f167c8
Zilong Liu <38041882+zilongliu-geo@users.noreply.github.com>
2022-02-22 23:32:54 -0800

    Fix autocomplete search by matching the typed first letters

--- dr-app/public/js/results_search.js
+++ dr-app/public/js/results_search.js
@@ -659,7 +659,16 @@ kwgApp.directive('zipDirective', function() {
             getZipCodeArea().then(function(data) {
                 if (element[0] == angular.element('#placeFacetsZip')[0] | element[0] == angular.element('#regionFacetsZip')[0]) {
                     element.autocomplete({
-                        source: Object.keys(data['zipcodes']),
+                        source: function(request, response)
+                        {
+                            var matches = $.map(Object.keys(data['zipcodes']), function(item){
+                                if (item.indexOf(request.term) === 0)
+                                {
+                                    return item;
+                                }
+                            });
+                            response(matches);
+                        },
                         select: function(event, ui) {
                             ngModelCtrl.$setViewValue(ui.item);
                             scope.$apply();
@@ -680,7 +689,16 @@ kwgApp.directive('uscdDirective', function() {
             getUSClimateDivision().then(function(data) {
                 if (element[0] == angular.element('#placeFacetsUSCD')[0] | element[0] == angular.element('#regionFacetsUSCD')[0]) {
                     element.autocomplete({
-                        source: Object.keys(data['divisions']),
+                        source: function(request, response)
+                        {
+                            var matches = $.map(Object.keys(data['divisions']), function(item){
+                                if (item.toUpperCase().indexOf(request.term.toUpperCase()) === 0)
+                                {
+                                    return item;
+                                }
+                            });
+                            response(matches);
+                        },
                         select: function(event, ui) {
                             ngModelCtrl.$setViewValue(ui.item);
                             scope.$apply();
@@ -700,8 +718,16 @@ kwgApp.directive('nwzDirective', function() {
             getNWZone().then(function(data) {
                 if (element[0] == angular.element('#placeFacetsNWZ')[0] | element[0] == angular.element('#regionFacetsNWZ')[0]) {
                     element.autocomplete({
-                        source: Object.keys(data['nwzones']),
-                        select: function(event, ui) {
+                        source: function(request, response)
+                        {
+                            var matches = $.map(Object.keys(data['nwzones']), function(item){
+                                if (item.toUpperCase().indexOf(request.term.toUpperCase()) === 0)
+                                {
+                                    return item;
+                                }
+                            });
+                            response(matches);
+                        },                        select: function(event, ui) {
                             ngModelCtrl.$setViewValue(ui.item);
                             scope.$apply();
                         }
